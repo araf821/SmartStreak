97194b215f933cd6413070f7392d17d887871e1d
araf821 <araf.ahmed200@gmail.com>
2023-10-29 02:20:27 -0400

    implemented profile creation + finalized setup page

--- components/ProfileCreationForm.tsx
+++ components/ProfileCreationForm.tsx
@@ -5,31 +5,41 @@ import FileUpload from "./FileUpload";
 import { useClerk, useUser } from "@clerk/nextjs";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useEffect } from "react";
-import { Form, FormControl, FormField, FormItem, FormLabel } from "./ui/form";
+import { useEffect, useState } from "react";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
 import { Input } from "./ui/input";
 import { RadioGroup, RadioGroupItem } from "./ui/radio-group";
 import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+import axios from "axios";
 
 const formSchema = z.object({
   name: z.string().min(3).max(16),
   imageUrl: z.string().min(3),
   profileType: z.enum(["STUDENT", "TEACHER"]),
-  grade: z.string().min(1).max(12),
+  grade: z.coerce.number().min(1).max(12),
 });
 
 const ProfileCreationForm = () => {
   const { user } = useUser();
   const { signOut } = useClerk();
   const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       name: "",
       imageUrl: "",
-      grade: "1",
-      profileType: 'STUDENT',
+      grade: 1,
+      profileType: "STUDENT",
     },
   });
 
@@ -42,7 +52,17 @@ const ProfileCreationForm = () => {
   }, [form, user]);
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    console.log(values);
+    setIsLoading(true);
+    try {
+      await axios.post("/api/profile", values);
+      toast.success("Profile created successfully!");
+      router.push("/dashboard");
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong.");
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
@@ -70,14 +90,17 @@ const ProfileCreationForm = () => {
             control={form.control}
             render={({ field }) => (
               <FormItem className="pt-4 space-y-1">
-                <FormLabel className="text-base">Your name</FormLabel>
+                <FormLabel className="text-base lg:text-lg font-medium">
+                  Your name
+                </FormLabel>
                 <FormControl>
                   <Input
-                    className="w-full text-base font-semibold"
+                    className="w-full text-slate-300 text-base font-semibold"
                     placeholder="Full Name"
                     {...field}
                   />
                 </FormControl>
+                <FormMessage />
               </FormItem>
             )}
           />
@@ -87,18 +110,19 @@ const ProfileCreationForm = () => {
             control={form.control}
             render={({ field }) => (
               <FormItem className="pt-4 space-y-1">
-                <FormLabel className="text-base">Your grade</FormLabel>
+                <FormLabel className="text-base lg:text-lg font-medium">
+                  Your grade
+                </FormLabel>
                 <FormControl>
                   <Input
                     type="number"
-                    className="w-full text-base font-semibold"
+                    className="w-full text-slate-300 text-base font-semibold"
                     placeholder="7"
-                    max={12}
-                    min={1}
                     {...field}
                     onChange={(event) => field.onChange(event.target.value)}
                   />
                 </FormControl>
+                <FormMessage />
               </FormItem>
             )}
           />
@@ -122,13 +146,19 @@ const ProfileCreationForm = () => {
                   >
                     <FormItem className="flex items-center space-x-3 space-y-0">
                       <FormControl>
-                        <RadioGroupItem value="STUDENT" />
+                        <RadioGroupItem
+                          className="text-red-600"
+                          value="STUDENT"
+                        />
                       </FormControl>
                       <FormLabel className="text-base">Student</FormLabel>
                     </FormItem>
                     <FormItem className="flex items-center space-x-3 space-y-0">
                       <FormControl>
-                        <RadioGroupItem value="TEACHER" />
+                        <RadioGroupItem
+                          className="text-red-600"
+                          value="TEACHER"
+                        />
                       </FormControl>
                       <FormLabel className="text-base">Teacher</FormLabel>
                     </FormItem>
@@ -145,11 +175,7 @@ const ProfileCreationForm = () => {
             >
               Cancel
             </button>
-            <button
-              type="button"
-              className="w-32 transition hover:text-white hover:bg-indigo-500 border-2 border-indigo-500 rounded-lg text-indigo-500 font-semibold text-lg py-1 px-2"
-              onClick={() => signOut(() => router.push("/"))}
-            >
+            <button className="w-32 transition hover:text-white hover:bg-indigo-500 border-2 border-indigo-500 rounded-lg text-indigo-500 font-semibold text-lg py-1 px-2">
               Create
             </button>
           </div>
